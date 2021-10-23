d5d8149fb55bf862cb8a7ddfa144139ca9bbd4a3
phonpitsanu <phonpitsanu@gmail.com>
2021-10-23 12:28:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
-# app.py
-from flask import Flask, request, jsonify
-from songline import Sendline
+from flask import Flask, request, abort, jsonify, json
+import requests
+import time
 
-token = 'xxxxxxxxxxxxxxxxx' 
+url = 'https://notify-api.line.me/api/notify'
+token = 'xxxxxxxxxxxxxx'
+headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+imange = 'https://raw.githubusercontent.com/phonpitsanu/line-notify-bot-webhook/main/doge.png'
 
 app = Flask(__name__)
+@app.route('/webhook', methods=['POST'])  
+def webhook():
+    if request.method == 'POST':
+        data = request.get_json()
+        alerts = data.get('alerts', '')
+        text = data.get('text', '')
+        #payload = {'message':alerts, 'imageThumbnail':imange, 'imageFullsize':imange}
+        payload = {'message':text+alerts}
+        r = requests.post(url, headers=headers, data = payload)
+        print (r.text)
+        return jsonify(payload)
+    else:
+        abort(400)
 
-@app.route('/webhook/', methods=['POST'])
-def post_webhook():
-    data = request.get_json()
-    name = data['name']
-    messenger = Sendline(token)
-    messenger.sendtext('Tradingview Triggered !!! ' +name)
-    return jsonify({'result': 'Success', 'name': name})
-
+    
 @app.route('/')
 def index():
-    return "<h1>BOT v202110 running !!!</h1>"
+    return "<h1>UNMAI BOT v2(line-notify-api) running !!!</h1>"
 
 if __name__ == '__main__':
     app.run()
